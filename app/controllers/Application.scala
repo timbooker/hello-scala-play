3c922891d497da060b3e36c3496c8c61be9bfb75
Timothy Booker <tim@vexeddigital.com>
2013-06-01 19:39:40 +0100

    Made a basic todo list

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,17 +2,38 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application TADSDSAKLJDASLDJASJDLAD is ready."))
+val taskForm = Form(
+  "label" -> nonEmptyText
+)
+import models.Task
+
+	def index = Action {
+  Redirect(routes.Application.tasks)
+}
+
+	def tasks = Action {
+	  Ok(views.html.index(Task.all(), taskForm))
+	}
+	
+	
+  def newTask = Action { 
+    implicit request => taskForm.bindFromRequest.fold(
+            errors => BadRequest(views.html.index(Task.all(), errors)),
+    label => {
+      Task.create(label)
+      Redirect(routes.Application.tasks)
+    }
+    )
   }
   
-  def newPage = Action { 
-	  Ok(views.html.index("Test123"))
-  }
-  
-  def tasks = TODO
+  def deleteTask(id: Long) = Action {
+	  Task.delete(id)
+	  Redirect(routes.Application.tasks)
+	}
   
 }
